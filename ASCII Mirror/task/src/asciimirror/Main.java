f7208bf13f4a24d57922ff4738a095370bb0e86a
danifoj315 <danifoj315@student.polsl.pl>
2023-04-09 22:06:41 +0200

    cow mocking part finished

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -1,19 +1,17 @@
 package asciimirror;
+
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
-            System.out.println("Input the file path:");
-            Scanner scanner = new Scanner(System.in);
-            String filePath = scanner.nextLine();
-            System.out.println(filePath);
-            // System.out.println("                    _______ ");
-            // System.out.println("                   < hello >");
-            // System.out.println("                    ------- ");
-            System.out.println("            ^__^            ");
-            System.out.println("    _______/(oo)            ");
-            System.out.println("/\\/(       /(__)            ");
-            System.out.println("   | w----||                ");
-            System.out.println("   ||     ||                ");
+    public static void main(String[] args) {
+        System.out.println("Input the file path:");
+        Scanner scanner = new Scanner(System.in);
+        String filePath = scanner.nextLine().trim();
+        System.out.println(filePath);
+        System.out.println("            ^__^");
+        System.out.println("    _______/(oo)");
+        System.out.println("/\\/(       /(__)");
+        System.out.println("   | w----||");
+        System.out.println("   ||     ||");
     }
 }

890dee3d23f2c30023236b3064939352b1fa8437
danifoj315 <danifoj315@student.polsl.pl>
2023-04-11 23:09:31 +0200

    added option to print twice the same output from file

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -1,7 +1,10 @@
 package asciimirror;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,21 +12,24 @@ public class Main {
         System.out.println("Input the file path:");
         Scanner scanner = new Scanner(System.in);
         String filePath = scanner.nextLine();
+        List<String> lines = new ArrayList<>();
+        int maxCharsInLine = 0;
         try {
             File file = new File(filePath);
             FileReader fileReader = new FileReader(file);
-            while(file.canRead()){
-                int character = fileReader.read();
-                if(character == -1){
-                    break; // End of file has been reached
-                }
-                String fileOutput = String.valueOf((char) character);
-                System.out.print(fileOutput);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+                maxCharsInLine = Math.max(maxCharsInLine, line.length());
             }
-
-            // Do something with file (e.g. read, write, etc.)
+            for (String lineItem : lines) {
+                String modifiedLine = String.format("%-" + maxCharsInLine + "s", lineItem);
+                System.out.println(modifiedLine + " | " + modifiedLine);
+            }
         } catch (Exception e) {
             System.out.println("File not found!");
         }
+        System.out.println("\n");
     }
 }
